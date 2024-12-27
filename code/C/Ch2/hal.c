04b294bcfcb70c27eadd2be51c1d93764c653bed
kross11480 <hritam.dutta@th-nuernberg.de>
2024-12-27 23:04:24 +0100

    * 9.c led chaser app * updated hal (mistake in BSRR)

---
+++ code/C/Ch2/hal.c
@@ -0,0 +1,84 @@
+#include "hal.h"
+
+void gpio_set_mode(GPIO_typeDef *gpio, uint16_t pin, moder_t mode)
+{
+    gpio->MODER &= ~(3<<(2*pin));
+    gpio->MODER |= (mode<<(2*pin));
+}
+
+void gpio_write(GPIO_typeDef *gpio, uint16_t pin, sig_t val)
+{
+    //low 16 bit odr high, upper 16 bit odr low
+        gpio->BSRR = ((1 << pin))<<( val ? 0: 16);
+}
+
+void gpio_toggle(GPIO_typeDef *gpio, uint16_t pin)
+{
+    gpio->ODR ^= (1 << pin);
+}
+
+void gpio_set_pupd(GPIO_typeDef *gpio, uint16_t pin, pupdr_t pupd)
+{
+    gpio->PUPDR &= ~(3<<(2*pin));
+    gpio->PUPDR |= (pupd<<(2*pin));
+}
+
+sig_t gpio_read(GPIO_typeDef *gpio, uint16_t pin)
+{
+    return (gpio->IDR & (1<<pin) ? HIGH : LOW);
+}
+
+void delay_ms(uint32_t ms) {
+    uint32_t count = ms * 1865; //Approx Factor
+    while (count--)
+        __asm__("NOP"); //6-8 Cycles per loop => @16 Mhz
+}
+
+void systick_init()
+{
+    SysTick->CTRL |= (1 << 2); //1: Clock 0: Clock/8.
+    SysTick->LOAD = 0xFFFFFF;
+}
+
+uint32_t systick_start()
+{
+    SysTick->CTRL |= (1 << 0); //start timer
+    uint32_t start = SysTick->VAL;
+    return start;
+}
+
+void systick_stop()
+{
+    SysTick->CTRL |= (0 << 0); //stop timer
+}
+
+void systick_delay_ms(uint32_t start, uint32_t time_in_ms)
+{
+    while (systick_get_millis(start) < time_in_ms);
+}
+
+uint32_t systick_get_millis(uint32_t start)
+{
+    uint32_t current_time = SysTick->VAL;
+    if (current_time < start)
+        return (start - current_time)/TICKS_PER_MS;
+    else
+        return (start + (0xFFFFFF - current_time))/TICKS_PER_MS;
+}
+
+sig_t debounce_button(GPIO_typeDef *gpio, uint16_t pin)
+{
+    static uint8_t debounce_state = HIGH; //stable state of button
+    static uint8_t last_button_reading = HIGH;
+
+    uint8_t button_current_state = gpio_read(gpio, pin);
+    if (button_current_state != last_button_reading) {
+        delay_ms(DEBOUNCE_TIME_MS);
+        if(button_current_state == gpio_read(gpio, pin))
+        {
+            debounce_state = button_current_state;
+        }
+    }
+    last_button_reading = button_current_state;
+    return debounce_state;
+}
---
+++ code/C/Ch2/hal.h
@@ -0,0 +1,58 @@
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+typedef enum {LOW, HIGH} sig_t;
+typedef enum {MODER_INPUT, MODER_OUTPUT, MODER_AF, MODER_ANALOG} moder_t;
+typedef enum {NONE, PULL_UP, PULL_DOWN} pupdr_t;
+
+typedef struct {
+    volatile uint32_t MODER;    // 0x00: mode register
+    volatile uint32_t OTYPER;   // 0x04: GPIO output type register
+    volatile uint32_t OSPEEDR;  // 0x08: GPIO output speed register
+    volatile uint32_t PUPDR;    // 0x0C: GPIO pull-up/pull-down register
+    volatile uint32_t IDR;      // 0x10: GPIO input data register
+    volatile uint32_t ODR;      // 0x14: GPIO output data register
+    volatile uint32_t BSRR;     // 0x18: GPIO bit set/reset register
+    volatile uint32_t LCKR;     // 0x1C: GPIO lock register
+    volatile uint32_t AFR[2];   // 0x20: GPIO alternate function registers
+} GPIO_typeDef;
+
+typedef struct {
+    uint32_t REGS_NOTNEEDEDNOW[19];
+    uint32_t AHB2ENR;  // AHB2 peripheral clock enable register
+} RCC_typeDef;
+
+typedef struct {
+    uint32_t CTRL;
+    uint32_t LOAD;
+    uint32_t VAL;
+} SysTick_t;
+
+/* Base Address of Peripheral */
+#define GPIOA ((GPIO_typeDef *) 0x48000000)
+#define GPIOB ((GPIO_typeDef *) 0x48000400)
+
+#define RCC ((RCC_typeDef *) 0x40021000) //Operator precedence remove ()
+#define SysTick ((SysTick_t *) 0xE000E010)
+
+
+
+void gpio_set_mode(GPIO_typeDef *gpio, uint16_t pin, moder_t mode);
+void gpio_set_pupd(GPIO_typeDef *gpio, uint16_t pin, pupdr_t pupd);
+void gpio_write(GPIO_typeDef *gpio, uint16_t pin, sig_t val);
+sig_t gpio_read(GPIO_typeDef *gpio, uint16_t pin);
+void gpio_toggle(GPIO_typeDef *gpio, uint16_t pin);
+
+void delay_ms(uint32_t ms);
+
+#define TICKS_PER_MS 16000
+#define TICKS_PER_US 16
+#define  DEBOUNCE_TIME_MS 10
+
+void systick_init();
+uint32_t systick_start();
+void systick_stop();
+uint32_t systick_get_millis(uint32_t start);
+void systick_delay_ms(uint32_t start, uint32_t time_in_ms);
+
+sig_t debounce_button(GPIO_typeDef *gpio, uint16_t pin);
