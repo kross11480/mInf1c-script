eba6099ee4d88975e419bdb8fb307e49d36f8675
kross11480 <hritam.dutta@th-nuernberg.de>
2024-12-27 23:03:46 +0100

    * 9.c led chaser app

--- code/C/Ch2/9.c
+++ code/C/Ch2/9.c
@@ -2,11 +2,13 @@
 //No blocking delay, (with debouncing)
 #include "hal.h"
 
-typedef enum {OFF, TOGGLE} state_t;
+typedef enum {OFF, LED_CHASER} state_t;
+#define NUM_LEDS 4
+#define PERIOD 250
 
 void main(void){
-    GPIO_typeDef *port_led = GPIOA;
-    uint16_t pin_led = 1; //LEDx connected to PAx in StefiLite
+    GPIO_typeDef *port_led[NUM_LEDS] = {GPIOA, GPIOA, GPIOA, GPIOA};
+    uint16_t pin_led[NUM_LEDS] = {0, 1, 2, 3};
 
     GPIO_typeDef *port_button = GPIOB;
     uint16_t pin_button = 7; //Button_x connected to PBx in StefiLite
@@ -16,14 +18,18 @@ void main(void){
     state_t app_state = OFF;
 
     uint32_t now = 0;
+    uint16_t j = 0;
+    int dir = 1;
 
     //initialize rcc, gpio, timer
     RCC->AHB2ENR |= 0x1; //Enable Clock for GPIOA
     RCC->AHB2ENR |= 0x2; //Enable Clock for GPIOB
 
     // set output mode for led pin and switch off led
-    gpio_set_mode(port_led, pin_led, MODER_OUTPUT);
-    gpio_write(port_led, pin_led, HIGH);
+    for(uint32_t i = 0; i < NUM_LEDS; i++){
+    gpio_set_mode(port_led[i], pin_led[i], MODER_OUTPUT);
+    gpio_write(port_led[i], pin_led[i], HIGH);
+    }
 
     //set input mode for push button
     gpio_set_mode(port_button, pin_button, MODER_INPUT);
@@ -37,7 +43,7 @@ void main(void){
         //Transition: if button is pressed once (edge detection), change app state
         if((button_state != prev_button_state) && (button_state == LOW))
         {
-            app_state = (app_state == OFF)? TOGGLE : OFF;
+            app_state = (app_state == OFF)? LED_CHASER : OFF;
             now = systick_start();
         }
 
@@ -45,14 +51,32 @@ void main(void){
         switch (app_state)
         {
         case OFF:
-            gpio_write(GPIOA, pin_led, HIGH);
-            break;
-        case TOGGLE:
-            if(systick_get_millis(now) >= 500)
+            for(uint32_t i = 0; i < NUM_LEDS; i++)
             {
-                gpio_toggle(GPIOA, pin_led);
+                gpio_write(port_led[i], pin_led[i], HIGH);
+            }
+            j = 1;
+            dir = -1;
+            break;
+        case LED_CHASER:
+            if(systick_get_millis(now) >= PERIOD)
+            {
+                j += dir;
+
+                gpio_write(port_led[j], pin_led[j], LOW);
+                gpio_write(port_led[j-dir], pin_led[j-dir], HIGH);
+
                 // reset timer
                 now = systick_start();
+                //update direction of LEDs
+                if(j == NUM_LEDS - 1)
+                {
+                    dir = -1;
+                }
+                if(j == 0)
+                {
+                    dir = 1;
+                }
             }
             break;
         }
