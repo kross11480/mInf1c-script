5bcaee865d0b4c21abc057336c7041ae5ca6d960
kross11480 <hritam.dutta@th-nuernberg.de>
2024-12-28 23:45:20 +0100

    * 9.c led chaser app * updated hal (mistake in BSRR)

--- code/ASM/toggle_led.s
+++ code/ASM/toggle_led.s
@@ -2,42 +2,40 @@
 .cpu cortex-m4
 .thumb
 
+.equ gpio_clock_address, 0x4002104C
+.equ led_address, 0x48000014
+.equ gpio_mode_address, 0x48000000
+.equ delay_count, 0x10000
+
 .word 0
 .word _start + 1
 .global _start
 _start:
-#Initialize Clock for GPIOC
-    ldr r0, =0x00000004
-    ldr r1, =0x4002104C
+#Initialize Clock
+    ldr r0, =0x00000001
+    ldr r1, =gpio_clock_address
     str r0, [r1]
 
-#Initialize LED 3
-#- switch LED 3(Port GPIO C7) off: bit value 1 is LED OFF, bit value 0 is LED ON
-#- OFF/ON: GPIOC -> ODR |= (1<<7); GPIOC->ODR &= ~( 1 << 7 );
-    ldr r1, =0x48000814
-    ldr r0, [r1]
-    orr r0, 0x0000080
-    str r0, [r1]
-
+#Initialize LED 0
 #- LEDs: GPIOC->MODER = (GPIOC->MODER & 0xFFFF00FF) | 0x00004000;
-    ldr r1, =0x48000800
+    ldr r1, =gpio_mode_address
     ldr r0, [r1]
-    and r0, 0xFFFF3FFF
-    orr r0, 0x00004000
+    and r0, 0xFFFFFFFC
+    orr r0, 0x00000001
     str r0, [r1]
 
 toggle:
 #Toggle LED
-    ldr r1, =0x48000814
+    ldr r1, =led_address
     ldr r0, [r1]
-    eor r0, 0x00000F0
+    eor r0, 0x0000001
     str r0, [r1]
     bl delay
     b toggle
 
 delay:
     movs    r6, #0
-    ldr     r7, =0x10000
+    ldr     r7, =0x100000
 .L1:
     adds    r6, r6, #1
     cmp     r6, r7
