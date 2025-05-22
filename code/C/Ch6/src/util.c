72e0e93262c87b116a997f203c746fdd4b8ba983
kross11480 <hritam.dutta@th-nuernberg.de>
2025-05-22 16:51:38 +0200

    removed folder from repo

--- code/C/Ch6/src/util.c
+++
@@ -1,7 +0,0 @@
-#include "libstefi/util.h"
-
-void inline soft_delay_ms(uint32_t time_in_ms) {
-    uint32_t count = time_in_ms * 1865; //Approx Factor
-    while (count--)
-        __asm__("NOP"); //6-8 Cycles per loop => @16 Mhz
-}
