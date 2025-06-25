c98ffc31c48e717b1cc90b752e91f668a3ce527b
kross11480 <hritam.dutta@th-nuernberg.de>
2025-06-25 12:01:10 +0200

    (feat) reaction game * always enable interrupt after registering callback, otherwise firing of interrupt will lead to undefined behavior * random nuber generator

---
+++ code/C/Ch6/libstefi/examples/5-ReactionGame/reaction_game.c
@@ -0,0 +1,76 @@
+#include <libstefi/rng.h>
+#include <libstefi/timer.h>
+#include <libstefi/uart.h>
+
+#include "stdio.h"
+#include "led.h"
+#include "button.h"
+#include "debounce.h"
+
+#define TIMER_ID TIMER3
+
+
+void s0_action() {
+    //wait a random timer
+    led_off(LED0_RED);
+    timer_start(TIMER_ID);
+    //random time between 0.5 and 1s
+    volatile uint32_t rnd_time = 500 + rng_get_rand()/(0x1 << 20);
+    while(timer_getcount(TIMER_ID) <= rnd_time);
+    timer_stop(TIMER_ID);
+    // start game
+    led_on(LED0_RED);
+    // start reaction timer
+    timer_start(TIMER_ID);
+}
+
+void s1_action() {
+    led_off(LED0_RED);
+    printf("Reaction Time: %lu \r\n", timer_getcount(TIMER_ID));
+    timer_stop(TIMER_ID);
+}
+
+void s0_callback() {
+    debounce_start(BUTTON_S0, s0_action);
+}
+
+
+void s1_callback() {
+    debounce_start(BUTTON_S1, s1_action);
+}
+
+void timer_expires() {
+    printf("Slow Finger \r\n");
+    timer_stop(TIMER_ID);
+}
+
+void setup() {
+    //initialize button
+    rng_init();
+    button_init(BUTTON_S0);
+    button_interrupt_init(BUTTON_S0, s0_callback);
+
+    button_init(BUTTON_S1);
+    button_interrupt_init(BUTTON_S1, s1_callback);
+    //initialize led
+    led_init(LED0_RED);
+    //initialize reaction timer
+    timer_init(TIMER_ID);
+    timer_set_period(TIMER_ID, 4000, 5000);
+    timer_interrupt_register_handler(TIMER_ID, timer_expires);
+    timer_enable_interrupt(TIMER_ID);
+}
+
+void main() {
+    //initialize uart
+    uart_configure();
+    printf("\r\n Start Reaction Game \r\n");
+
+    setup();
+
+    //initialize debounce timer (TIM4)
+    debounce_init();
+
+    while(1) {
+    }
+}
--- code/C/Ch6/libstefi/src/rng.c
+++ code/C/Ch6/libstefi/src/rng.c
@@ -7,14 +7,13 @@
 void rng_init() {
     //initialize rcc clock
     peripheral_rng_enable();
-
     // Enable RNG
     RNG->CR |= BIT(2);
 }
 
 uint32_t rng_get_rand() {
     // Wait for data ready, Error handling later
-    while (RNG->SR & BIT(0) == 0);
+    while ((RNG->SR & BIT(0)) == 0);
 
     // Return the random number
     return RNG->DR;
