2366bafc24f9e3fbe6a0b53535d0f68efbec032d
kross11480 <hritam.dutta@th-nuernberg.de>
2025-08-02 08:26:53 +0200

    (refactor) tests * simple test to test apis, to be completed in a different branch

--- code/C/Ch7/libstefi/examples/4-TestShell/testshell.c
+++ code/C/Ch7/libstefi/examples/4-TestShell/testshell.c
@@ -6,12 +6,20 @@
 
 
 #define LED A5
-void main(){
+
+void gpio_init() {
     peripheral_gpio_enable(PERIPHERAL_GPIOA);
     gpio_set_mode(LED, MODER_OUTPUT); // LED1 on Port C pin 5 on STefi
+}
 
+void gpio_process() {
     while(1) {
         gpio_toggle(LED);
         soft_delay_ms(50);
     }
+}
+
+void main(){
+    gpio_init();
+    gpio_process();
 }
