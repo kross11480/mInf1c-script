53d59af52d8b499c4d1c0b6fbd512ea36b851fae
duttahr <hritam.dutta@th-nuernberg.de>
2025-08-04 13:15:48 +0200

    [Test] Test functions for GPIO * Checklist for GPIO API * EXTI10_15_IRQHandler was not in vector table, caught because the user button on nucleo is on PC13 * GPIO read/write tested * gpio interrupt tested by toggling user led with user button

--- code/C/Ch7/libstefi/examples/4-TestShell/testshell.c
+++ code/C/Ch7/libstefi/examples/4-TestShell/testshell.c
@@ -1,25 +1,58 @@
-// Task: Empty Template: turning on LED1 on libstefi
+// Task: test api functions on stm32 nucleo
 #include <libstefi/util.h>
 
 #include "libstefi/gpio.h"
 #include "libstefi/peripheral.h"
+#include "libstefi/uart.h"
 
 
 #define LED A5
+#define BUTTON C13
 
 void gpio_init() {
     peripheral_gpio_enable(PERIPHERAL_GPIOA);
-    gpio_set_mode(LED, MODER_OUTPUT); // LED1 on Port C pin 5 on STefi
+    peripheral_gpio_enable(PERIPHERAL_GPIOC);
+    gpio_set_mode(LED, MODER_OUTPUT);
+    gpio_set_mode(BUTTON, MODER_INPUT);
+    gpio_set_pupd(BUTTON, PULL_UP);
 }
 
-void gpio_process() {
+void gpio_toggle_test() {
     while(1) {
         gpio_toggle(LED);
-        soft_delay_ms(50);
+        soft_delay_ms(500);
+    }
+}
+
+void gpio_readwrite_test()
+{
+    // USER LED on Nucleo Board in High-Active
+    gpio_write(LED, LOW);
+    if(gpio_read(LED) == HIGH)
+    {
+        printf("GPIO read/write test passed \r\n");
+    } else
+    {
+        printf("GPIO read/write test failed\r\n");
+    }
+}
+
+void button_interrupt_callback()
+{
+    gpio_toggle(LED);
+}
+
+void gpio_interrupt_test() {
+    gpio_enable_interrupt(BUTTON, FALLING_EDGE);
+    gpio_interrupt_register_handler(BUTTON, button_interrupt_callback);
+    while (1) {
     }
 }
 
 void main(){
+    uart_configure();
     gpio_init();
-    gpio_process();
+    //gpio_toggle_test();
+    //gpio_readwrite_test();
+    gpio_interrupt_test();
 }
